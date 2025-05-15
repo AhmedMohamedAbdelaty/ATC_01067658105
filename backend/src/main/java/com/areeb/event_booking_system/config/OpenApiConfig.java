87250d003a2c98a584f1b3fc1f82c4855078e11e
Ahmed Muhammed Abdelaty <am0103738@gmail.com>
2025-05-15 19:06:15 +0300

    feat(api): add Swagger UI and OpenAPI configuration for API documentation

---
+++ backend/src/main/java/com/areeb/event_booking_system/config/OpenApiConfig.java
@@ -0,0 +1,42 @@
+package com.areeb.event_booking_system.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .addServersItem(new Server().url("/"))
+                .info(new Info()
+                        .title("Event Booking System API")
+                        .description("API documentation for Event Booking System application")
+                        .version("v1.0")
+                        .contact(new Contact()
+                                .name("Ahmed")
+                                .email("ahmedmo.contact@gmail.com"))
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("https://www.apache.org/licenses/LICENSE-2.0")))
+                .addSecurityItem(new SecurityRequirement().addList("JWT"))
+                .components(new Components()
+                        .addSecuritySchemes("JWT",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                                        .in(SecurityScheme.In.HEADER)
+                                        .name("Authorization")));
+    }
+}
